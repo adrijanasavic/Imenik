51701fe48da2eb6bed2f47e64434b487df4bb8bc
Adrijana <adrijana.jovicic@gmail.com>
2020-05-19 17:56:47 +0200

    menu-MainActivity

--- app/src/main/java/com/example/imenik/activities/MainActivity.java
+++ app/src/main/java/com/example/imenik/activities/MainActivity.java
@@ -9,6 +9,8 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -36,6 +38,33 @@ public class MainActivity extends AppCompatActivity {
         fillDataDrawer();
         setupToolbar();
         setupDrawer();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate( R.menu.menu, menu );
+        return super.onCreateOptionsMenu( menu );
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create:
+                setTitle( "Novi kontakt" );
+                break;
+            case R.id.action_settings:
+                setTitle( "Podesavanja" );
+                break;
+            case R.id.action_about:
+                setTitle( "O aplikaciji" );
+                break;
+        }
+
+        return super.onOptionsItemSelected( item );
+    }
+
+    public void updateTitle(String text) {
+        setTitle( text );
     }
 
     private void fillDataDrawer() {
